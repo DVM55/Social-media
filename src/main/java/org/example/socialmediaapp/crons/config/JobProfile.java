24206ac26d0268c9bdbe0672dfaf865dbd74436b
manhdzai1105 <duongvanmanh11052003@gmail.com>
2026-02-23 14:07:34 +0700

    feat: config quartz

---
+++ src/main/java/org/example/socialmediaapp/crons/config/JobProfile.java
@@ -0,0 +1,13 @@
+package org.example.socialmediaapp.crons.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobProfile {
+    private String name;
+    private String cron;
+}
