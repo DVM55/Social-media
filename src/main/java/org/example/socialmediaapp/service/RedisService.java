36841449eb3c2b5700b3eccb659c4a27adbb9629
manhdzai1105 <duongvanmanh11052003@gmail.com>
2026-02-23 14:02:29 +0700

    config redis

---
+++ src/main/java/org/example/socialmediaapp/service/RedisService.java
@@ -0,0 +1,29 @@
+package org.example.socialmediaapp.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void set(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public void setWithTTL(String key, Object value, long seconds) {
+        redisTemplate.opsForValue().set(key, value, seconds, TimeUnit.SECONDS);
+    }
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+}
