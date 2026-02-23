e10102cc9f5284c560de541b63b2e1031d060865
manhdzai1105 <duongvanmanh11052003@gmail.com>
2026-02-23 13:54:52 +0700

    init project

---
+++ src/main/java/org/example/socialmediaapp/SocialMediaAppApplication.java
@@ -0,0 +1,18 @@
+package org.example.socialmediaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+public class SocialMediaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaAppApplication.class, args);
+    }
+
+}
